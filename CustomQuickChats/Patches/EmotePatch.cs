ed8bdd6e5f3e835c09969114bac2e7c3c6ae5a20
CDela <CDela@DESKTOP-7AF8NV9>
2025-05-15 01:41:27 -0400

    Add project files.

---
+++ CustomQuickChats/Patches/EmotePatch.cs
@@ -0,0 +1,136 @@
+﻿using HarmonyLib;
+
+namespace CustomQuickChats
+{
+    [HarmonyPatch(typeof(EmoteSystem), "EmoteConverter")]
+    internal class EmotePatch
+    {
+        static void Postfix(ref string __result, EmoteMeanings emote)
+        {
+            if (!Plugin.Enabled.Value) return;
+
+            switch (emote)
+            {
+                case EmoteMeanings.EMOTE_OverHere:
+                    __result = Plugin.OverHereMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_HurryUp:
+                    __result = Plugin.HurryUpMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_No:
+                //    __result = Plugin.NoMsg.Value;
+                //    break;
+                case EmoteMeanings.EMOTE_TooEasy:
+                    __result = Plugin.TooEasyMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Thinking:
+                    __result = Plugin.ThinkingMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_OMG:
+                    __result = Plugin.OMGMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_Yeah:
+                //    __result = Plugin.YeahMsg.Value;
+                //    break;
+                case EmoteMeanings.EMOTE_GoodIdea:
+                    __result = Plugin.GoodIdeaMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_WellDone:
+                    __result = Plugin.WellDoneMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_SoClose:
+                    __result = Plugin.SoCloseMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_NotThatOne:
+                    __result = Plugin.NotThatOneMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Impossible:
+                    __result = Plugin.ImpossibleMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_Higher:
+                //    __result = Plugin.HigherMsg.Value;
+                //    break;
+                //case EmoteMeanings.EMOTE_Lower:
+                //    __result = Plugin.LowerMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_GlueHere:
+                    __result = Plugin.GlueHereMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Bomb:
+                    __result = Plugin.BombMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Rematch:
+                    __result = Plugin.RematchMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_WaitingForAFriend:
+                    __result = Plugin.WaitingForAFriendMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_BeRightBack:
+                    __result = Plugin.BeRightBackMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Okay:
+                    __result = Plugin.OkayMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Hello:
+                    __result = Plugin.HelloMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Goodbye:
+                    __result = Plugin.GoodbyeMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_GoodGame:
+                    __result = Plugin.GoodGameMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_WellPlayed:
+                    __result = Plugin.WellPlayedMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_NotThere:
+                    __result = Plugin.NotThereMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_MoreTraps:
+                    __result = Plugin.MoreTrapsMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_NiceOutfit:
+                    __result = Plugin.NiceOutfitMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_Yes:
+                //    __result = Plugin.YesMsg.Value;
+                //    break;
+                //case EmoteMeanings.EMOTE_UhOh:
+                //    __result = Plugin.UhOhMsg.Value;
+                //    break;
+                case EmoteMeanings.EMOTE_Hahaha:
+                    __result = Plugin.HahahaMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_Ouch:
+                //    __result = Plugin.OuchMsg.Value;
+                //    break;
+                case EmoteMeanings.EMOTE_Nooo:
+                    __result = Plugin.NooooMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_Amazing:
+                //    __result = Plugin.AmazingMsg.Value;
+                //    break;
+                case EmoteMeanings.EMOTE_Wow:
+                    __result = Plugin.WowMsg.Value;
+                    break;
+                //case EmoteMeanings.EMOTE_GreatRun:
+                //    __result = Plugin.GreatRunMsg.Value;
+                //    break;
+                case EmoteMeanings.EMOTE_Thanks:
+                    __result = Plugin.ThanksMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Whoops:
+                    __result = Plugin.WhoopsMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_Sorry:
+                    __result = Plugin.SorryMsg.Value;
+                    break;
+                case EmoteMeanings.EMOTE_NoProblem:
+                    __result = Plugin.NoProblemMsg.Value;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ CustomQuickChats/Plugin.cs
@@ -0,0 +1,118 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CustomQuickChats
+{
+    [BepInPlugin("customquickchats.killacon", "Custom Quick Chats", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> Enabled;
+
+        public static ConfigEntry<string> OverHereMsg;
+        public static ConfigEntry<string> HurryUpMsg;
+        public static ConfigEntry<string> NoMsg;
+        public static ConfigEntry<string> TooEasyMsg;
+        public static ConfigEntry<string> ThinkingMsg;
+        public static ConfigEntry<string> OMGMsg;
+        public static ConfigEntry<string> YeahMsg;
+        public static ConfigEntry<string> GoodIdeaMsg;
+        public static ConfigEntry<string> WellDoneMsg;
+        public static ConfigEntry<string> SoCloseMsg;
+        public static ConfigEntry<string> NotThatOneMsg;
+        public static ConfigEntry<string> ImpossibleMsg;
+        public static ConfigEntry<string> HigherMsg;
+        public static ConfigEntry<string> LowerMsg;
+        public static ConfigEntry<string> GlueHereMsg;
+        public static ConfigEntry<string> BombMsg;
+        public static ConfigEntry<string> RematchMsg;
+        public static ConfigEntry<string> WaitingForAFriendMsg;
+        public static ConfigEntry<string> BeRightBackMsg;
+        public static ConfigEntry<string> OkayMsg;
+        public static ConfigEntry<string> HelloMsg;
+        public static ConfigEntry<string> GoodbyeMsg;
+        public static ConfigEntry<string> GoodGameMsg;
+        public static ConfigEntry<string> WellPlayedMsg;
+        public static ConfigEntry<string> NotThereMsg;
+        public static ConfigEntry<string> MoreTrapsMsg;
+        public static ConfigEntry<string> NiceOutfitMsg;
+        public static ConfigEntry<string> YesMsg;
+        public static ConfigEntry<string> UhOhMsg;
+        public static ConfigEntry<string> HahahaMsg;
+        public static ConfigEntry<string> OuchMsg;
+        public static ConfigEntry<string> NooooMsg;
+        public static ConfigEntry<string> AmazingMsg;
+        public static ConfigEntry<string> WowMsg;
+        public static ConfigEntry<string> GreatRunMsg;
+        public static ConfigEntry<string> ThanksMsg;
+        public static ConfigEntry<string> WhoopsMsg;
+        public static ConfigEntry<string> SorryMsg;
+        public static ConfigEntry<string> NoProblemMsg;
+
+        internal ManualLogSource mls;
+        private static Plugin Instance;
+
+        private void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("CustomQuickChats");
+            mls.LogInfo("killacon da goat frfr :speaking_head: :100:");
+            
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            Enabled = Config.Bind("General", "Enabled", true, "Enable or disable custom quick chats");
+
+            OverHereMsg = Config.Bind("QuickChats", "OverHere", "Over here!", "");
+            TooEasyMsg = Config.Bind("QuickChats", "TooEasy", "Too easy.", "");
+            ThinkingMsg = Config.Bind("QuickChats", "Thinking", "Thinking...", "");
+            ImpossibleMsg = Config.Bind("QuickChats", "Impossible", "Impossible!", "");
+            RematchMsg = Config.Bind("QuickChats", "Rematch", "Rematch?", "");
+            WaitingForAFriendMsg = Config.Bind("QuickChats", "WaitingForAFriend", "Waiting for a friend.", "");
+            BeRightBackMsg = Config.Bind("QuickChats", "BeRightBack", "Be right back.", "");
+
+            OMGMsg = Config.Bind("QuickChats", "OMG", "OMG!", "");
+            WowMsg = Config.Bind("QuickChats", "Wow", "Wow!", "");
+            HahahaMsg = Config.Bind("QuickChats", "Hahaha", "Hahaha!", "");
+            NooooMsg = Config.Bind("QuickChats", "Noooo", "Noooo!", "");
+            OkayMsg = Config.Bind("QuickChats", "Okay", "Okay!", "");
+            NotThatOneMsg = Config.Bind("QuickChats", "NotThatOne", "Not that one!", "");
+            HurryUpMsg = Config.Bind("QuickChats", "HurryUp", "Hurry up!", ""); 
+
+            //explitive here
+            WhoopsMsg = Config.Bind("QuickChats", "Whoops", "Whoops...", ""); 
+            SorryMsg = Config.Bind("QuickChats", "Sorry", "Sorry!", "");
+            HelloMsg = Config.Bind("QuickChats", "Hello", "Hello!", ""); 
+            GoodbyeMsg = Config.Bind("QuickChats", "Goodbye", "Goodbye", "");
+            NiceOutfitMsg = Config.Bind("QuickChats", "NiceOutfit", "Nice Outfit!", "");
+            NoProblemMsg = Config.Bind("QuickChats", "NoProblem", "No problem!", "");
+
+            ThanksMsg = Config.Bind("QuickChats", "Thanks", "Thanks!", "");
+            WellDoneMsg = Config.Bind("QuickChats", "WellDone", "Well done!", "");
+            GoodIdeaMsg = Config.Bind("QuickChats", "GoodIdea", "Good idea!", "");
+            GoodGameMsg = Config.Bind("QuickChats", "GoodGame", "Good game!", ""); 
+            WellPlayedMsg = Config.Bind("QuickChats", "WellPlayed", "Well played!", ""); 
+            SoCloseMsg = Config.Bind("QuickChats", "SoClose", "So close!", "");
+
+            NotThereMsg = Config.Bind("QuickChats", "NotThere", "Not there!", "");
+            MoreTrapsMsg = Config.Bind("QuickChats", "MoreTraps", "More traps!", "");
+            GlueHereMsg = Config.Bind("QuickChats", "GlueHere", "Glue here!", "");
+            BombMsg = Config.Bind("QuickChats", "Bomb", "Bomb!", "");
+
+            //YeahMsg = Config.Bind("QuickChats", "Yeah", "Yeah!", "");
+            //HigherMsg = Config.Bind("QuickChats", "Higher", "Higher!", "");
+            //LowerMsg = Config.Bind("QuickChats", "Lower", "Lower!", "");
+            //YesMsg = Config.Bind("QuickChats", "Yes", "Yes!", "");
+            //UhOhMsg = Config.Bind("QuickChats", "UhOh", "Uh oh!", "");
+            //OuchMsg = Config.Bind("QuickChats", "Ouch", "Ouch!", "");
+            //AmazingMsg = Config.Bind("QuickChats", "Amazing", "Amazing!", "");
+            //GreatRunMsg = Config.Bind("QuickChats", "GreatRun", "Great run!", "");
+            //NoMsg = Config.Bind("QuickChats", "No", "Noooo!", "");
+
+            Harmony harmony = new Harmony("customquickchats.killacon");
+            harmony.PatchAll();
+        }
+    }
+}
